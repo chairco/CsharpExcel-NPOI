4f25a515b43da4712d7d0aa2212b2055021fa2f8
yichieh_chen <yichieh_chen@pegatroncorp.com>
2014-07-09 17:14:37 +0800

    get set function

--- sample/Program.cs
+++ sample/Program.cs
@@ -7,7 +7,7 @@ using System.IO;
 using NPOI.HSSF.Util;
 using NPOI.HSSF.UserModel;
 using System.Linq;
-using System.Collections.Generic;
+using System.Data;
 using Excel = Microsoft.Office.Interop.Excel;
 
 
@@ -21,45 +21,20 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace sample
 {
-    class Program
+    public class ModelInfo
     {
-        public void _openexcel(string Datasource)
-        {
-            using (FileStream fs = new FileStream(Datasource, FileMode.Open, FileAccess.Read))
-            {
-                IWorkbook wb;
+        public decimal FieldA { get; set; }
+        public decimal FieldB { get; set; }
+        public decimal FieldC { get; set; }
+    }
 
-                if (Datasource.Contains(".xlsx"))
-                    wb = new XSSFWorkbook(fs);
-                else
-                    wb = new HSSFWorkbook(fs);
-            }
-        }
+    public class SheetModifyProductList
+    {
+        public string setProductNumber { get; set; }
+    }
 
-        public void openexcel(string Datasource)
-        {
-            XSSFWorkbook wk2007;
-            XSSFSheet st2007;
-            HSSFWorkbook wk2003;
-            HSSFSheet st2003;
-            
-            using (FileStream fs = new FileStream(Datasource, FileMode.Open, FileAccess.Read))
-            {
-                if (Datasource.Contains(".xlsx")) //2007
-                {
-                    wk2007 = new XSSFWorkbook(fs);
-                    st2007 = (XSSFSheet)wk2007.GetSheetAt(0);
-                }
-                else //2003
-                {
-                    wk2003 = new HSSFWorkbook(fs);
-                    st2003 = (HSSFSheet)wk2003.GetSheetAt(0);
-                }
-
-                fs.Close();
-            }
-        }
-
+    class EXCEL
+    {
         private List<SheetModifyProductList> sheetMappingToList(XSSFSheet sheet)
         {
             List<SheetModifyProductList> modelList = new List<SheetModifyProductList>();
@@ -67,7 +42,8 @@ namespace sample
             {
                 SheetModifyProductList model = new SheetModifyProductList();
                 if (sheet.GetRow(i).GetCell(0) != null)
-                    model.setProductNumber(sheet.GetRow(i).GetCell(0).ToString().Trim());
+                    //model.setProductNumber(sheet.GetRow(i).GetCell(0).ToString().Trim());
+                    model.setProductNumber = sheet.GetRow(i).GetCell(0).ToString().Trim();
 
                 modelList.Add(model);
             }
@@ -81,8 +57,8 @@ namespace sample
             {
                 SheetModifyProductList model = new SheetModifyProductList();
                 if (sheet.GetRow(i).GetCell(0) != null)
-                    model.setProductNumber(sheet.GetRow(i).GetCell(0).ToString().Trim());
-
+                    //model.setProductNumber(sheet.GetRow(i).GetCell(0).ToString().Trim());
+                    model.setProductNumber = sheet.GetRow(i).GetCell(0).ToString().Trim();
                 modelList.Add(model);
             }
             return modelList;
@@ -117,10 +93,101 @@ namespace sample
             }
             return count;
         }
+    }
+
+    class Program
+    {
+        public void openexcel(string Datasource)
+        {
+            IWorkbook wk;
+            ISheet st;
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            
+            using (FileStream fs = new FileStream(Datasource, FileMode.Open, FileAccess.Read))
+            {
+                if (Datasource.Contains(".xlsx")) //2007
+                {
+                    wk = new XSSFWorkbook(fs);
+                    st = (XSSFSheet)wk.GetSheetAt(0);
+                }
+                else //2003
+                {
+                    wk = new HSSFWorkbook(fs);
+                    st = (HSSFSheet)wk.GetSheetAt(0);
+                }
+                int sheetCount = wk.NumberOfSheets;
+                
+                for (int k = 0; k < sheetCount; k++) //讀出sheetname
+                {
+                    var hs = wk.GetSheetAt(k); //sheet
+                    string sheetname = hs.SheetName; //sheet's name
+                    
+                    if (sheetname != "N61 FF") continue;
+                    
+                    var hr = hs.GetRow(0); //row
+                    for (int i = hs.FirstRowNum; i <= hs.LastRowNum; i++)
+                    {
+                        hr = hs.GetRow(i); //column
+                        for (int j = hr.FirstCellNum; j < hr.LastCellNum; j++)
+                        {
+                            if (hr.GetCell(j) != null)
+                            {
+                                //string strcell = hr.GetCell(i) == null ? "0" : hr.GetCell(i).ToString();
+                                //Console.Write("({0},{1})={2} ; ", i, j, strcell);
+                                Console.Write("({0},{1}) = {2} ; ", i, j, hr.GetCell(j).ToString());
+                            }
+                        }
+                        Console.WriteLine("\t\n");
+                    }
+                }
+                wk = null; //全部Sheet讀完關閉Excel
+                fs.Close();
+                Console.Read();
+            }
+        }
 
         static void Main(string[] args)
-        {
+        { 
             
         }
+
+        /***
+        static void Main(string[] args)
+        {
+            //new一個用List接的Model
+            List<ModelInfo> ModelInfoList = new List<ModelInfo>();
+
+            //會被覆蓋的問題在此，因為只new一次！
+            //ModelInfo isModelInfo = new ModelInfo();
+
+            decimal addA = (Decimal)0.1, addB = (Decimal)0.2, addC = (Decimal)0.3;
+
+            //跑三次迴圈
+            for (int cnt = 0; cnt <= 3; cnt++)
+            {
+                ModelInfo isModelInfo = new ModelInfo();
+                addA++;
+                addB++;
+                addC++;
+
+                //每次迴圈Model的每個欄位都增加新的資訊
+                isModelInfo.FieldA = addA;
+                isModelInfo.FieldB = addB;
+                isModelInfo.FieldC = addC;
+
+                //將每次迴圈紀錄的欄位Model，add到List接的Model
+                ModelInfoList.Add(isModelInfo);
+            }
+            
+            foreach (var item in ModelInfoList)
+            {
+                Console.WriteLine(item.FieldA);
+                Console.WriteLine(item.FieldB);
+                Console.WriteLine(item.FieldC);
+            }
+            Console.Read();
+        }
+         *///
     }
 }
